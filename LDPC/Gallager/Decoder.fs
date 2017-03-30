30b48079de45c2db3b3f0de8f664104fa42a9189
Jan Schiefer <jschiefer@users.noreply.github.com>
2017-03-30 01:57:34 -0700

    Restructure for real-world testing

---
+++ LDPC/Gallager/Decoder.fs
@@ -0,0 +1,21 @@
+﻿module Hamstr.Decoder
+
+open System
+open System.IO
+open Hamstr.Ldpc.DvbS2
+
+let testDataFileName = "/home/jan/Docker/qpsk_testdata.out"
+
+let readFrame frametype modulation stream =
+    let nBits = bitsPerFrame frametype
+    let bps = bitsPerSymbol modulation
+    ()
+
+let readLongQpskFrame stream = readFrame Long M_QPSK stream
+
+[<EntryPoint>]
+let main argv =
+    use stream = File.OpenRead(testDataFileName)
+    use reader = new StreamReader(stream)
+    let frame = readLongQpskFrame stream
+    0 // return an integer exit code
