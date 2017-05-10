2649386ea7595785f6ba7c810ea513075da096a6
Jan Schiefer <jschiefer@users.noreply.github.com>
2017-05-10 09:59:38 -0700

    Fix user name

--- scripts/startp4g.ps1
+++ scripts/startp4g.ps1
@@ -1,1 +1,1 @@
-& docker run -it --rm --privileged -p 5901:5901 -e USER=amsat p4g:0.3 /bin/bash -c "vncserver :1 -geometry 1280x800 -depth 24 && tail -F /root/.vnc/*.log"
+& docker run -it --rm --privileged -p 5901:5901 -e USER=amsat p4g:0.4 /bin/bash -c "vncserver :1 -geometry 1280x800 -depth 24 && tail -F /root/.vnc/*.log"
