acb12b2056dd262ac8cd663a0169c5f26604b050
Jan Schiefer <jschiefer@users.noreply.github.com>
2017-10-31 01:23:01 -0700

    Clearnup som more Windows debris

--- LDPC/Gallager/script.fsx
+++ LDPC/Gallager/script.fsx
@@ -1,13 +1,11 @@
-// #r "bin/Debug/netcoreapp1.1/Gallager.dll"
-
-#r "bin/Debug/Gallager2.exe"
+#r "bin/Debug/netcoreapp1.1/Gallager.dll"
 
 open FSharp.Numerics
 open Hamstr.Ldpc.DvbS2
 open Hamstr.Ldpc.Decoder
 open Hamstr.Ldpc.Main
 
-let iqDataFileName = "c:/Users/jas/Develop/Github/p4g-toys/LDPC/Data/qpsk_testdata.out"
+let iqDataFileName = "../Data/qpsk_testdata.out"
 let message = readTestFile IqFile iqDataFileName Long ModCodLookup.[04uy]
 let nBitnodes = 64800
 let nChecknodes = 32400
