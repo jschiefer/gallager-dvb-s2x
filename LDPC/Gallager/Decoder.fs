bc3a8b5081e42efedac2d50b47cbee110882661f
Jan Schiefer <jschiefer@users.noreply.github.com>
2017-10-31 01:12:20 -0700

    Cleanup windows stuff

--- LDPC/Gallager/Decoder.fs
+++ LDPC/Gallager/Decoder.fs
@@ -3,6 +3,32 @@
 open FSharp.Numerics
 open Hamstr.Ldpc.DvbS2
 
+type Contribution = {
+    /// Who contributed this
+    peerIndex : int
+    llr : LLR
+}
+
+/// LDPC-decode the frame (which is an array of tuples of bit and LLR)
+type BitNode = {
+    /// My bitnode index
+    index : int
+    /// List of indices of the associated checkNodes
+    checkNodes : int list
+    /// Current sum of the modulo-2 additions
+    value : LLR
+    // List of contributions from the peers
+    contributions : Contribution list
+}
+
+type CheckNode = {
+    /// My checknode index
+    index : int
+    /// List of indices of the associated bitNodes
+    bitNodes : int list
+    // List of contributions from the peers
+    contributions : Contribution list
+}
 
 // Compile the connections between data nodes and check nodes
 let makeDecodeTables typeAndCode =
@@ -21,54 +47,34 @@ let makeDecodeTables typeAndCode =
                 let dataOffset = blockNo * 360 + blockOffset
                 line
                 |> Array.map (fun accAddress -> 
-                    // For each element in the accumulator line, modulo-2 add the data bit to the parity accumulator
+                    // Each element in the line, is a parity accumulator that this bit goes into 
                     let parityIndex = 
-                        (accAddress + (dataOffset % 360) * codingTableEntry.q ) % nParityBits
+                        (accAddress + (dataOffset % 360) * codingTableEntry.q) % nParityBits
                     (dataOffset, parityIndex))))
             |> List.concat
         |> Array.concat
 
-    // Treat parity bits as data. Kudos to g4guo for the insight!
-    let parityLinks = 
-        seq { for i in 0 .. nParityBits - 1 do yield (nDataBits + i, nDataBits + i)} 
-        |> Array.ofSeq
-
-    // This handles the final XOR during encoding
+    // Handle the final XOR in the encoding. Thank you to g4guo for the insight!
     let xorLinks = 
-        seq { for i in 0 .. nParityBits - 2 do yield (nDataBits + i, nDataBits + i + 1)} 
+        seq { for i in 1 .. nParityBits - 2 do yield (i + nDataBits, i - 1) }  
         |> Array.ofSeq
 
-    // Split up the linkage between bitNodes and checkNodes
-    let bitNodes = Array.create nBitNodes ([] : int list) 
-    let checkNodes = Array.create nParityBits ([] : int list)
+    // Create separate index lists for bitnodes and checknodes
+    let bitNodePeerLists = Array.create nBitNodes ([] : int list) 
+    let checkNodePeerLists = Array.create nParityBits ([] : int list)
 
-    [| accumulatorLinks; parityLinks; xorLinks |] 
+    [| accumulatorLinks; xorLinks |] 
     |> Array.concat 
     |> Array.iter (fun (b, c) -> 
-        bitNodes.[b] <- c :: bitNodes.[b]
-        checkNodes.[c] <- b :: checkNodes.[c])
+        bitNodePeerLists.[b] <- c :: bitNodePeerLists.[b]
+        checkNodePeerLists.[c] <- b :: checkNodePeerLists.[c])
+
+    let bitNodes = bitNodePeerLists |> Array.mapi (fun i c -> 
+        { index = i; checkNodes = c; value = LLR.Undecided; contributions = [] })
+    let checkNodes = checkNodePeerLists |> Array.mapi (fun i b -> 
+        { index = i; bitNodes = b; contributions = [] })
+    
     (bitNodes, checkNodes)
-
-/// LDPC-decode the frame (which is an array of tuples of bit and LLR)
-type BitNode = {
-    /// List of indices of the associated checkNodes
-    checkNodes : int list
-    /// Current sum of the modulo-2 additions
-    sum : LLR
-}
-
-type CheckNode = {
-    /// List of indices of the associated bitNodes
-    bitNodes : int list
-    /// Current sum of the modulo-2 additions
-    sum : LLR
-}
-
-type Foo = {
-    /// Index to BitNode or CheckNode
-    nodeId : int
-    llrContribution : LLR
-}
 
 let decode typeAndCode frame =
     let processBitnodes frame bitnodes = 
