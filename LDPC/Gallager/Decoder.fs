757bb7bca333f2545e75fa1ff9e5a6b206f663c4
Jan Schiefer <jschiefer@users.noreply.github.com>
2017-11-07 08:15:49 -0800

    Migrate decoder into library

--- LDPC/Gallager/Decoder.fs
+++ LDPC/Gallager/Decoder.fs
@@ -77,15 +77,63 @@ let makeDecodeTables typeAndCode =
     (bitNodes, checkNodes)
 
 let decode typeAndCode frame =
-    let processBitnodes frame bitnodes = 
 
-        ()
-    let processCheckNodes frame = 
-        ()
-    let checkParityEquations frame = 
-        ()
+    // Initialize bitnodes from message bits
+    let initializeBitNodes (frame : FECFRAME) (bitnodes : BitNode []) = 
+        bitnodes 
+            |> Array.map (fun b -> 
+                { b with value = frame.bits.[b.index] })
 
-    let (bitNodes, checkNodes) = makeDecodeTables typeAndCode
+    // Update checknodes by adding contributions from all the connected bit nodes
+    let updateCheckNodes (checknodes : CheckNode[]) (bitnodes : BitNode[]) = 
+        checknodes
+        |> Array.map (fun c ->
+            let contris = 
+                c.bitNodes 
+                |> List.map (fun b -> 
+                    { peerIndex = b; llr = bitnodes.[b].value })
+            { c with contributions = contris } )
 
+    // Update bitnodes by assembling contributions from connected checknodes
+    let updateBitnodes (bitnodes : BitNode[]) (checknodes : CheckNode[]) =
+        let summarizeChecknode c exclude =
+            c.contributions
+            |> List.filter (fun cont -> cont.peerIndex <> exclude) 
+            |> List.map (fun cont -> cont.llr)
+            |> List.fold (+) LLR.Undecided
 
-    [ 0uy; 0uy ]
+        bitnodes
+        |> Array.map (fun b ->
+            let contris = 
+                b.checkNodes
+                |> List.map (fun i -> checknodes.[i]) 
+                |> List.map (fun c -> { peerIndex = c.index; llr = summarizeChecknode c b.index } )
+            { b with contributions = contris } )
+
+    // Compute hard decision
+    let computeHardDecision (bitnodes : BitNode []) =
+        bitnodes 
+        |> Array.map (fun b -> 
+            let hardDecision = 
+                b.contributions
+                |> List.map (fun cont -> cont.llr)
+                |> List.fold (+) LLR.Undecided
+            { b with value = hardDecision } )
+
+    // Check parity equations: The sum of all the bitnodes adjacent to a 
+    // checknode must be 0.
+    let checkParityEquations (checknodes : CheckNode []) (bitnodes : BitNode []) =
+        let nonzeros = 
+            checknodes
+            |> Array.map (fun c -> 
+                c.contributions
+                |> List.map (fun bi -> bitnodes.[bi.peerIndex].value.ToBool)
+                |> List.fold (<>) false) 
+            |> Array.filter not
+            |> Array.length
+        nonzeros = 0
+
+    let (blankBitnodes, checkNodes) = makeDecodeTables typeAndCode
+    let bitnodes = initializeBitNodes frame blankBitnodes
+
+    ()
