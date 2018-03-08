6fd4f7a22ce20b735fe2c69d714c28ba8b602067
Jan Schiefer <jschiefer@users.noreply.github.com>
2018-03-08 00:59:56 -0800

    Different computation of XOR

--- LDPC/Gallager/Decoder.fs
+++ LDPC/Gallager/Decoder.fs
@@ -57,7 +57,7 @@ let makeDecodeTables typeAndCode =
     // Handle the final XOR in the encoding. Thank you to g4guo for the insight!
     // [FIXME] This needs verification, as the offsets are very strange.
     let xorLinks = 
-        seq { for i in 1 .. nParityBits - 2 do yield (i + nDataBits, i - 1) }  
+        seq { for i in 0 .. nParityBits - 1 do yield (i + nDataBits, i) }  
         |> Array.ofSeq
 
     // Create separate index lists for bitnodes and checknodes
@@ -77,63 +77,64 @@ let makeDecodeTables typeAndCode =
     
     (bitNodes, checkNodes)
 
-let decode typeAndCode frame =
 
-    // Initialize bitnodes from message bits
-    let initializeBitNodes (frame : FECFRAME) (bitnodes : BitNode []) = 
-        bitnodes 
-            |> Array.map (fun b -> 
-                { b with value = frame.bits.[b.index] })
-
-    // Update checknodes by adding contributions from all the connected bit nodes
-    let updateCheckNodes (bitnodes : BitNode[]) (checknodes : CheckNode[]) = 
-        checknodes
-        |> Array.map (fun c ->
-            let contris = 
-                c.bitNodes 
-                |> List.map (fun b -> 
-                    { peerIndex = b; llr = bitnodes.[b].value })
-            { c with contributions = contris } )
-
-    // Update bitnodes by assembling contributions from connected checknodes
-    let updateBitnodes (bitnodes : BitNode[]) (checknodes : CheckNode[]) =
-        let summarizeChecknode c exclude =
-            c.contributions
-            |> List.filter (fun cont -> cont.peerIndex <> exclude) 
-            |> List.map (fun cont -> cont.llr)
-            |> List.fold (+) LLR.Undecided
-
-        bitnodes
-        |> Array.map (fun b ->
-            let contris = 
-                b.checkNodes
-                |> List.map (fun i -> checknodes.[i]) 
-                |> List.map (fun c -> { peerIndex = c.index; llr = summarizeChecknode c b.index } )
-            { b with contributions = contris } )
-
-    // Compute hard decision
-    let computeHardDecision (bitnodes : BitNode []) =
-        bitnodes 
+// FIXME: Decoder will start here
+// Initialize bitnodes from message bits
+let initializeBitNodes (frame : FECFRAME) (bitnodes : BitNode []) = 
+    bitnodes 
         |> Array.map (fun b -> 
-            let hardDecision = 
-                b.contributions
-                |> List.map (fun cont -> cont.llr)
-                |> List.fold (+) LLR.Undecided
-            { b with value = hardDecision } )
+            { b with value = frame.bits.[b.index] })
 
-    // Check parity equations: The sum of all the bitnodes adjacent to a 
-    // checknode must be 0.
-    let checkParityEquations (bitnodes : BitNode []) (checknodes : CheckNode []) =
-        let nonzeros = 
-            checknodes
-            |> Array.map (fun c -> 
-                c.contributions
-                |> List.map (fun bi -> bitnodes.[bi.peerIndex].value.ToBool)
-                |> List.fold (<>) false) 
-            |> Array.filter not
-            |> Array.length
-        nonzeros = 0
+// Update checknodes by adding contributions from all the connected bit nodes
+let updateCheckNodes (bitnodes : BitNode[]) (checknodes : CheckNode[]) = 
+    checknodes
+    |> Array.map (fun c ->
+        let contris = 
+            c.bitNodes 
+            |> List.map (fun b -> 
+                { peerIndex = b; llr = bitnodes.[b].value })
+        { c with contributions = contris } )
 
+// Update bitnodes by assembling contributions from connected checknodes
+let updateBitnodes (bitnodes : BitNode[]) (checknodes : CheckNode[]) =
+    let summarizeChecknode c exclude =
+        c.contributions
+        |> List.filter (fun cont -> cont.peerIndex <> exclude) 
+        |> List.map (fun cont -> cont.llr)
+        |> List.reduce (+) 
+
+    bitnodes
+    |> Array.map (fun b ->
+        let contris = 
+            b.checkNodes
+            |> List.map (fun i -> checknodes.[i]) 
+            |> List.map (fun c -> { peerIndex = c.index; llr = summarizeChecknode c b.index } )
+        { b with contributions = contris } )
+
+// Compute hard decision
+let computeHardDecision (bitnodes : BitNode []) =
+    bitnodes 
+    |> Array.map (fun b -> 
+        let hardDecision = 
+            b.contributions
+            |> List.map (fun cont -> cont.llr)
+            |> List.reduce (+)
+        { b with value = hardDecision } )
+
+// Check parity equations: The sum of all the bitnodes adjacent to a 
+// checknode must be 0.
+let checkParityEquations (bitnodes : BitNode []) (checknodes : CheckNode []) =
+    let nonzeros = 
+        checknodes
+        |> Array.map (fun c -> 
+            c.contributions
+            |> List.map (fun bi -> bitnodes.[bi.peerIndex].value.ToBool)
+            |> List.reduce (<>) ) 
+        |> Array.filter not
+        |> Array.length
+    nonzeros = 0
+
+let decode typeAndCode frame =
     let (blankBitnodes, checkNodes) = makeDecodeTables typeAndCode
     let bitnodes = initializeBitNodes frame blankBitnodes
     let newChecknodes = updateCheckNodes bitnodes checkNodes 
--- LDPC/Gallager/script.fsx
+++ LDPC/Gallager/script.fsx
@@ -1,65 +1,8 @@
 #r "bin/Debug/netcoreapp1.1/Gallager.dll"
 
-open FSharp.Numerics
 open Hamstr.Ldpc.DvbS2
 open Hamstr.Ldpc.Decoder
 open Hamstr.Ldpc.Main
-
-
-// Initialize bitnodes from message bits
-let initializeBitNodes (frame : FECFRAME) (bitnodes : BitNode []) = 
-    bitnodes 
-        |> Array.map (fun b -> 
-            { b with value = frame.bits.[b.index] })
-
-// Update checknodes by adding contributions from all the connected bit nodes
-let updateCheckNodes (bitnodes : BitNode[]) (checknodes : CheckNode[]) = 
-    checknodes
-    |> Array.map (fun c ->
-        let contris = 
-            c.bitNodes 
-            |> List.map (fun b -> 
-                { peerIndex = b; llr = bitnodes.[b].value })
-        { c with contributions = contris } )
-
-// Update bitnodes by assembling contributions from connected checknodes
-let updateBitnodes (bitnodes : BitNode[]) (checknodes : CheckNode[]) =
-    let summarizeChecknode c exclude =
-        c.contributions
-        |> List.filter (fun cont -> cont.peerIndex <> exclude) 
-        |> List.map (fun cont -> cont.llr)
-        |> List.reduce (+) 
-
-    bitnodes
-    |> Array.map (fun b ->
-        let contris = 
-            b.checkNodes
-            |> List.map (fun i -> checknodes.[i]) 
-            |> List.map (fun c -> { peerIndex = c.index; llr = summarizeChecknode c b.index } )
-        { b with contributions = contris } )
-
-// Compute hard decision
-let computeHardDecision (bitnodes : BitNode []) =
-    bitnodes 
-    |> Array.map (fun b -> 
-        let hardDecision = 
-            b.contributions
-            |> List.map (fun cont -> cont.llr)
-            |> List.reduce (+)
-        { b with value = hardDecision } )
-
-// Check parity equations: The sum of all the bitnodes adjacent to a 
-// checknode must be 0.
-let checkParityEquations (bitnodes : BitNode []) (checknodes : CheckNode []) =
-    let nonzeros = 
-        checknodes
-        |> Array.map (fun c -> 
-            c.contributions
-            |> List.map (fun bi -> bitnodes.[bi.peerIndex].value.ToBool)
-            |> List.reduce (<>) ) 
-        |> Array.filter not
-        |> Array.length
-    nonzeros = 0
 
 let printBitNodes (bitnodes : BitNode[]) =
     bitnodes
@@ -67,32 +10,28 @@ let printBitNodes (bitnodes : BitNode[]) =
     |> Array.iteri (fun i b -> printfn "%A %A " i b.value)
 
 
+let dec typeAndCode frame =
+    let (blankBitnodes, checkNodes) = makeDecodeTables typeAndCode
+    let bitnodes = initializeBitNodes frame blankBitnodes
+    let newChecknodes = updateCheckNodes bitnodes checkNodes 
+    let newBitnodes = updateBitnodes bitnodes newChecknodes
+    let hd = computeHardDecision newBitnodes
+    checkParityEquations hd newChecknodes
+
+let dec2 = dec (Long, Rate_1_2)
+
 let iqDataFileName = "../Data/qpsk_testdata.out"
 let frame = readTestFile IqFile iqDataFileName Long ModCodLookup.[04uy]
-let typeAndCode = (Long, Rate_1_2)
-let (blankBitnodes, checkNodes) = makeDecodeTables typeAndCode
-let bitnodes = initializeBitNodes frame blankBitnodes
-let newChecknodes = updateCheckNodes bitnodes checkNodes 
-let newBitnodes = updateBitnodes bitnodes newChecknodes
-let hd = computeHardDecision newBitnodes
 
-printBitNodes  newBitnodes
 
-blankBitnodes.[64799]
+let b = frame |> dec2 
 
-let b = newBitnodes.[0]
-b.contributions
-|> List.map (fun cont -> cont.llr)
-|> List.reduce (+) 
 
-newBitnodes |> Array.filter (fun b -> b.contributions.IsEmpty) |> Array.length
+let nParityBits = 8
+let nDataBits = 8
+let origXorLinks = 
+    seq { for i in 1 .. nParityBits - 2 do yield (i + nDataBits, i - 1) }  |> Array.ofSeq
 
-// Bitnodes 32400 and 64799 do not have any contributions (they do have values, though)
-// The offsets between the index of the Bitnodes and the checkNodes seem off:
-// bit node 32400 -> no check node
-// bit node 32401 -> check node 0
-// bit node 32402 -> check node 1
-// ....
-// bit node 64798 -> check node 32397
-// bit node 64799 -> no check node
-// This looks like there are two off-by-one errors.
+let betterXorLinks =
+    seq { for i in 0 .. nParityBits - 1 do yield (i + nDataBits, i) }  |> Array.ofSeq
+
